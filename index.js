b5ed72c83a822b1392e766c5ea11cdc6f3bc01fb
twriad969 <157061362+twriad969@users.noreply.github.com>
2024-06-01 03:06:17 +0600

    Update index.js

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ bot.onText(/https:\/\/(1024terabox|teraboxapp|freeterabox)\.com\/s\//, async (ms
 const sendVerificationPrompt = async (msg) => {
     const userId = msg.from.id.toString();
     const uniqueId = `${userId}_${Date.now()}`;
-    const longUrl = `https://telegram.me/TeraboxAdsFreeBot?start=${uniqueId}`;
+    const longUrl = `https://telegram.me/TeraboxBZWBot?start=${uniqueId}`;
     const shortUrl = await getShortUrl(longUrl);
 
     const verifyButton = {
